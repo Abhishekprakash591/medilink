3c50af92985bf38db24487ad19fe04a04596dc7e
Jayadeep T P <tpjayadeep9@gmail.com>
2026-03-14 16:07:38 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import App from './App.jsx';
-import App from 'https://raw.githubusercontent.com/Abhishekprakash591/medilink/refs/heads/main/src/App.jsx'
-//import './styles/globals.css';
-import 'https://raw.githubusercontent.com/Abhishekprakash591/medilink/blob/main/src/styles/globals.css';
+import App from './App.jsx';
+//import App from 'https://raw.githubusercontent.com/Abhishekprakash591/medilink/refs/heads/main/src/App.jsx'
+import './styles/globals.css';
+//import 'https://raw.githubusercontent.com/Abhishekprakash591/medilink/blob/main/src/styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
